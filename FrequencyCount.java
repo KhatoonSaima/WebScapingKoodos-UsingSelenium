62ef7a30976985e41368c4c07e386e667388db26
Khatoonsaima <sksaim786@gmail.com>
2025-04-29 23:23:13 -0400

    Initial commit

---
+++ FrequencyCount.java
@@ -0,0 +1,7 @@
+public class FrequencyCount {
+
+        public static void main() throws FileNotFoundException {
+            Scanner scanner = new Scanner(new File("./MobilePlans.csv"));
+
+    }
+}
