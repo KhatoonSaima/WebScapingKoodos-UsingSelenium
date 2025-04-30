d82c36aa21880293652f2b5038313cff59eda32a
Saima Khatoon <40600184+KhatoonSaima@users.noreply.github.com>
2025-04-29 23:36:23 -0400

    Delete FrequencyCount.java

--- FrequencyCount.java
+++
@@ -1,7 +0,0 @@
-public class FrequencyCount {
-
-        public static void main() throws FileNotFoundException {
-            Scanner scanner = new Scanner(new File("./MobilePlans.csv"));
-
-    }
-}
